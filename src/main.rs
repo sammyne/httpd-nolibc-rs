704e9cc0f2402c90a55c95ae48531764f8c47f07
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-10-21 12:31:04 +0000

    feat: draft http impl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+use core::ptr;
+
 use ministd::env::{self, Args};
 use ministd::{prelude::*, process};
 
@@ -12,16 +14,15 @@ mod syscalls;
 use c::SockaddrIn;
 
 const AF_INET: u16 = 2;
-//const SOCK_STREAM 1
-//const IPPROTO_TCP 6
-//const SO_REUSEADDR 2
-//const SOL_SOCKET 1
-//const SHUT_RDWR 2
-//const O_RDONLY 0
+const SOCK_STREAM: i32 = 1;
+const IPPROTO_TCP: i32 = 6;
+const SO_REUSEADDR: i32 = 2;
+const SOL_SOCKET: i32 = 1;
+const SHUT_RDWR: i32 = 2;
+const O_RDONLY: i32 = 0;
 
 fn main(args: Args<'_>) {
     let (port, filename) = must_parse_args(args);
-    println(filename);
 
     let addr = SockaddrIn {
         sin_family: AF_INET,
@@ -30,14 +31,96 @@ fn main(args: Args<'_>) {
         ..Default::default()
     };
 
-    //loop {
-    //
-    //}
+    let yes: _ = 1i32.to_ne_bytes();
+
+    let sock = unsafe { must_tcp_listen(&addr, &yes) };
+    loop {
+        let conn = unsafe { syscalls::accept(sock, ptr::null(), 0) };
+        if conn < 0 {
+            perror("accept");
+            continue;
+        }
+
+        let pid = unsafe { syscalls::fork() };
+        if pid < 0 {
+            perror("fork");
+            continue;
+        } else if pid == 0 {
+            // todo: 处理错误码
+            unsafe { http_serve(conn, filename).expect("serve") };
+            return;
+        }
+    }
 
     //for v in args {
     //    let s = str::from_utf8_unchecked(v);
     //    println(s);
     //}
+}
+
+fn die(s: &str) -> ! {
+    print("FATAL: ");
+    println(s);
+    process::exit(1);
+}
+
+unsafe fn http_consume(fd: i32) {
+    let mut buf = [0u8; 8192];
+    loop {
+        let n = syscalls::read(fd, buf.as_mut_ptr(), buf.len());
+        if n < 0 {
+            perror("read");
+            return;
+        }
+        let n = n as usize;
+
+        let s = ministd::str::from_utf8_unchecked(&buf[..n]);
+        print(s);
+
+        if n < 3 || s.ends_with("\n\r\n") {
+            return;
+        }
+    }
+}
+
+unsafe fn http_drop(conn: i32) {
+    let _ = syscalls::shutdown(conn, SHUT_RDWR);
+    let _ = syscalls::close(conn);
+}
+
+unsafe fn http_serve(fd: i32, filename: &str) -> Result<(), i32> {
+    http_consume(fd);
+
+    // 假设 filename 源自 argv[i]，因此底层是合法的 C 字符串
+    let f = syscalls::open(filename.as_ptr() as *const i8, O_RDONLY);
+    if f < 0 {
+        perror("open");
+        writeln(fd, concat!("HTTP/1.1 404 NOT FOUND\r\n\r\n404 NOT FOUND"));
+        return Err(1);
+    }
+    writeln(fd, "HTTP/1.1 200 OK\r\n\r\n");
+
+    let mut buf = [0u8; 8192];
+    loop {
+        let n = match syscalls::read(f, buf.as_mut_ptr(), buf.len()) {
+            v if v < 0 => {
+                perror("read");
+                break;
+            }
+            0 => break,
+            n => n as usize,
+        };
+
+        let n = write(fd, &buf[..n]);
+        if n < 0 {
+            perror("write");
+            return Err(1);
+        }
+    }
+
+    http_drop(fd);
+
+    Ok(())
 }
 
 fn must_parse_args(args: Args<'_>) -> (u16, &str) {
@@ -64,6 +147,31 @@ fn usage(argv0: &str) {
 }
 
 // 待整理
-fn must_tcp_listen(addr: &SockaddrIn, opt: &[u8]) -> i32 {
-    todo!()
+unsafe fn must_tcp_listen(addr: &SockaddrIn, opt: &[u8]) -> i32 {
+    let sock = syscalls::socket(AF_INET as i32, SOCK_STREAM, IPPROTO_TCP);
+    if sock < 0 {
+        die("socket");
+    }
+
+    let err = syscalls::setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, opt);
+    if err != 0 {
+        die("setsockopt");
+    }
+
+    let err = syscalls::bind(sock, addr);
+    if err != 0 {
+        die("bind");
+    }
+
+    let err = syscalls::listen(sock, 10);
+    if err != 0 {
+        die("listen");
+    }
+
+    sock
+}
+
+fn perror(s: &str) {
+    print("ERROR: ");
+    println(s);
 }
--- src/ministd/prelude/mod.rs
+++ src/ministd/prelude/mod.rs
@@ -3,11 +3,27 @@ use crate::syscalls::{self, FILENO_STDOUT};
 pub fn print(s: &str) {
     let b = s.as_bytes();
     unsafe {
-        syscalls::write(FILENO_STDOUT, b.as_ptr(), b.len());
+        let _ = syscalls::write(FILENO_STDOUT, b.as_ptr(), b.len());
     }
 }
 
 pub fn println(s: &str) {
     print(s);
     print("\n");
+}
+
+pub fn write<T>(fd: i32, data: T) -> isize
+where
+    T: AsRef<[u8]>,
+{
+    let b = data.as_ref();
+    unsafe { syscalls::write(fd as u32, b.as_ptr(), b.len()) }
+}
+
+pub fn writeln<T>(fd: i32, data: T)
+where
+    T: AsRef<[u8]>,
+{
+    let _ = write(fd, data);
+    let _ = write(fd, &[b'\n']);
 }
