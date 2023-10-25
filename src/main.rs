18108083473c709cb51cd56cf283313ce4f7f8a0
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-10-25 01:47:33 +0000

    feat(ministd::fs): impl File

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main] // disable all Rust-level entry points
 
 use ministd::env::{self, Args};
+use ministd::fs::File;
 use ministd::net::{SocketAddrV4, TcpListener, TcpStream};
 use ministd::{prelude::*, process};
 
@@ -9,8 +10,6 @@ mod c;
 mod ministd;
 mod start;
 mod syscalls;
-
-const O_RDONLY: i32 = 0;
 
 fn main(args: Args<'_>) {
     let (addr, filename) = must_parse_args(args);
@@ -65,17 +64,19 @@ unsafe fn http_serve(conn: TcpStream, filename: &str) -> Result<(), i32> {
     http_consume(&conn);
 
     // 假设 filename 源自 argv[i]，因此底层是合法的 C 字符串
-    let f = syscalls::open(filename.as_ptr() as *const i8, O_RDONLY);
-    if f < 0 {
-        perror("open");
-        conn.write(b"HTTP/1.1 404 NOT FOUND\r\n\r\n404 NOT FOUND\n")
-            .map_err(|err| {
-                perror("write 'NOT FOUND' http header");
-                err
-            })?;
+    let f = match File::open(filename) {
+        Ok(f) => f,
+        Err(_) => {
+            perror("open");
+            conn.write(b"HTTP/1.1 404 NOT FOUND\r\n\r\n404 NOT FOUND\n")
+                .map_err(|err| {
+                    perror("write 'NOT FOUND' http header");
+                    err
+                })?;
 
-        return Err(1);
-    }
+            return Err(1);
+        }
+    };
     conn.write(b"HTTP/1.1 200 OK\r\n\r\n").map_err(|err| {
         perror("write http header");
         err
@@ -83,13 +84,13 @@ unsafe fn http_serve(conn: TcpStream, filename: &str) -> Result<(), i32> {
 
     let mut buf = [0u8; 8192];
     loop {
-        let n = match syscalls::read(f, buf.as_mut_ptr(), buf.len()) {
-            v if v < 0 => {
+        let n = match f.read(&mut buf) {
+            Ok(0) => break,
+            Ok(n) => n,
+            Err(_) => {
                 perror("read");
                 break;
             }
-            0 => break,
-            n => n as usize,
         };
 
         let _ = conn.write(&buf[..n]).map_err(|err| {
